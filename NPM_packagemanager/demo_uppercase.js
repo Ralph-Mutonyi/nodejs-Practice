e79835272bf262dc00f95e7df8aa894f30109bde
RALPH MUTONYI <ralfmutonyi@gmail.com>
2024-05-01 12:32:30 +0300

    Failed to use upper-case module

---
+++ NPM_packagemanager/demo_uppercase.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var uc = import('upper-case');
+
+http.createServer(function(req, res){
+    res.writeHead(200, {"Content-Type" : "text/html"});
+    res.write(str.toupperCase("Hello World"));
+    res.end();
+}).listen(8080);
